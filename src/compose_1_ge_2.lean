01464d7afc0914a279d76268f9a3f599e585f5c8
Julien de Castelnau <julien.decastelnau@epfl.ch>
2025-05-31 23:25:09 +0200

    progress

--- src/compose_1_ge_2.lean
+++ src/compose_1_ge_2.lean
@@ -1,5 +1,7 @@
 import src.trace
 
+import Mathlib.Order.Defs.LinearOrder
+
 namespace Compose1Ge2
 
 inductive WstState
@@ -44,8 +46,10 @@ lemma noloss_cond_los: ∀ (IB: InfB) (wst inp: ℕ → ℚ) (C: ℚ) (t: ℕ), 
   intros IB wst inp C t contr
   linarith [contr]
 
-lemma flat_monotonic: Monotone (fun _: ℕ => 0) := by
-  unfold Monotone
+def flat : ℕ → ℚ := fun _: ℕ => 0
+
+lemma flat_monotonic: Monotone flat := by
+  unfold Monotone flat
   intros
   simp
 
@@ -60,7 +64,84 @@ def t1_line (C1: ℚ) (t: ℕ) := (↑t * C1) - (t1_wst C1 t)
 lemma t1_wst_monotonic: Monotone (t1_wst t1_C) := by
   unfold Monotone t1_C
   intros x1 x2
-  induction x2 <;> intros Hx1 <;> sorry
+  induction x2 <;> intros Hx1
+  . cases x1 <;> linarith
+  . rename_i x2 IH
+    cases Hx1
+    . rfl
+    . rename_i H
+      specialize IH H
+      calc
+        _ ≤ t1_wst 2 x2 := IH
+        _ ≤ _ := by (unfold t1_wst; cases x2 <;> simp)
+
+lemma t1_line_monotonic: Monotone (t1_line t1_C) := by
+  unfold Monotone t1_C
+  intros x1 x2
+  induction x2 <;> intros Hx1
+  . cases x1 <;> linarith
+  . rename_i x2 IH
+    cases Hx1
+    . rfl
+    . rename_i H
+      specialize IH H
+      calc
+        _ ≤ t1_line 2 x2 := IH
+        _ ≤ _ := by {
+          unfold t1_line t1_wst
+          cases x2 <;> simp
+        }
+
+lemma t1_constraint_u: ∀ t: ℕ, (t1_line t1_C) t ≤ (t1_C) * ↑t - (t1_wst t1_C) t := by
+  unfold t1_line t1_wst t1_C
+  intros
+  simp
+  rw [mul_comm]
+
+lemma t1_constraint_l: ∀ t: ℕ, (t1_line t1_C) t ≥ (t1_C) * ↑(t - t_D) - (t1_wst t1_C) (t - t_D) := by
+  intros t
+  unfold t1_C t_D
+  cases' (le_or_gt t 3) with tle tgt
+  . unfold t1_line t1_wst
+    rw [<- Nat.sub_eq_zero_iff_le] at tle
+    rw [tle]
+    cases t <;> simp
+  . have h: (2 * ↑(t - 3) - t1_wst 2 (t - 3) = (t1_line 2 (t-3))) := by
+      unfold t1_line; simp; apply mul_comm
+    rw [h]
+    apply t1_line_monotonic
+    rw [@Nat.sub_le_iff_le_add]
+    linarith
+
+lemma t1_cond_waste: ∀ t: ℕ, (t1_wst t1_C) t < (t1_wst t1_C) (t + 1) -> t1_line t1_C (1 + t) - 0 <= t1_C * ↑(1 + t) - (t1_wst t1_C) (1 + t) := by
+  unfold t1_line t1_wst
+  intros t
+  cases t <;> simp
+
+def mkt1 (IB: InfB): Trace := by
+  refine Trace.mk t1_C IB.B t_D
+    (t1_line t1_C) (t1_line t1_C) (t1_wst t1_C) (λ t: ℕ => 0)
+    (?_) (?_)
+    IB.pos_B t1_constraint_u t1_constraint_l t1_cond_waste (?_) (?_) (?_) t1_line_monotonic t1_line_monotonic t1_wst_monotonic flat_monotonic (?_) (?_) (?_) (?_) <;> try simp
+  . unfold t1_C; simp
+  . apply IB.noloss
+  . unfold t1_line t1_wst; simp
+  . unfold t1_line t1_wst; simp
+  . unfold t1_wst; simp
+
+
+def t2_C := 1
+
+def t2_out (C1 : ℚ) (C2: ℚ) (t: ℕ): ℚ :=
+  if (t ≤ 2) then t1_line C1 t else C2 * ↑t
+
+lemma t2_constraint_u: ∀ t: ℕ, (t2_out t1_C t2_C) t ≤ (t2_C) * ↑t - 0 := by
+  unfold t2_out t1_line t1_wst t1_C t2_C
+  intros t
+  cases t <;> simp
+  rename_i t
+  cases (le_or_gt t 1)
+  .
 
 
 /-
